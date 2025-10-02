fc78fb5560db684be01b1f89b2573016848b45f7
Coral1786 <u21211786@utp.edu.pe>
2025-10-01 23:24:06 -0500

    Update DemoCommit.sql
    Corrección de la consulta debido a un error con la columna 'SpanishProductName'

--- DemoCommit.sql
+++ DemoCommit.sql
@@ -13,36 +13,67 @@ BEGIN
         INSERT INTO AdventureWorksDW2022.dbo.DimProduct
         (
             ProductAlternateKey,
+            ProductSubcategoryKey,
+            WeightUnitMeasureCode,
+            SizeUnitMeasureCode,
             EnglishProductName,
+            SpanishProductName,
+            FrenchProductName,
             StandardCost,
-            ListPrice,
             FinishedGoodsFlag,
+            Color,
             SafetyStockLevel,
             ReorderPoint,
+            ListPrice,
+            DealerPrice,
+            Size,
+            SizeRange,
+            Weight,
             DaysToManufacture,
+            ProductLine,
+            Class,
+            Style,
+            ModelName,
+            EnglishDescription,
+            FrenchDescription,
             StartDate,
             Status
         )
         VALUES
         (
             @ProductAlternateKey,
+            1,
+            'LB',
+            'CM',
             @EnglishProductName,
+            'Producto de prueba',
+            'Produit de test',
             @StandardCost,
+            1,
+            'Black',
+            500,
+            250,
             @ListPrice,
-            1,          
-            100,        
-            50,         
-            1,          
-            GETDATE(),  
+            @ListPrice - 5,
+            'M',
+            'Medium',
+            2.5,
+            3,
+            'S',
+            'H',
+            'U',
+            'Test Model',
+            'English description',
+            'Description en français',
+            GETDATE(),
             'Current'
         );
-                
+
         COMMIT TRANSACTION;
         PRINT 'Insert successful. Transaction committed.';
     END TRY
 
     BEGIN CATCH
-        
         IF @@TRANCOUNT > 0
             ROLLBACK TRANSACTION;
 
